1adf5731d30cf613aa82544076d0209ed4f0fe89
Vania <vania.baek@gmail.com>
2016-02-26 13:54:12 -0800

    Commenting out last row

--- memfree.lua
+++ memfree.lua
@@ -25,4 +25,4 @@ local swaptotal = meminfo.SwapTotal.size
 print(string.format("%18s %10s %10s %10s %10s %10s",'total', 'used', 'free', 'shared', 'buffers', 'cached'))
 print(string.format("Mem: %13d %10d %10d %10d %10d %10d", memtotal, memused, memfree, memshared, membuffers, memcached))
 --print(string.format("-/+ buffers/cache: %10d %10d", 1, 2))
-print(string.format("Swap: %12d %10d %10d", swaptotal, swapused, swapfree))
+--print(string.format("Swap: %12d %10d %10d", swaptotal, swapused, swapfree))

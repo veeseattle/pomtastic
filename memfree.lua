10886f0669b7f88ca87dfaeb6826cc58c3697ccb
Vania <vania.baek@gmail.com>
2016-02-26 13:48:20 -0800

    Add missing lines

--- memfree.lua
+++ memfree.lua
@@ -21,3 +21,8 @@ local membuffers = meminfo.Buffers.size
 local memcached = meminfo.Cached.size
 
 local swaptotal = meminfo.SwapTotal.size
+
+print(string.format("%18s %10s %10s %10s %10s %10s",'total', 'used', 'free', 's$
+print(string.format("Mem: %13d %10d %10d %10d %10d %10d", memtotal, memused, me$
+--print(string.format("-/+ buffers/cache: %10d %10d", 1, 2))
+print(string.format("Swap: %12d %10d %10d", swaptotal, swapused, swapfree))
